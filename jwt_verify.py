f10deccf4d65f696e44b209947831953d8a4e59c
emmaguo13 <emma@proofofalpha.io>
2023-01-08 15:20:13 -0800

    working python

--- jwt_verify.py
+++ jwt_verify.py
@@ -5,6 +5,8 @@ import os
 import base64
 
 def verifyJWT(header, payload, sig, pub_key):
+
+    # sig = "mLCysHQtDftfFey4F-ntFma22r5-qpxtkXsiDw6TY30Tnoj2kPQ_YdSjzagrwRgF7pHE8SSM_roo2wDh3c_8vDNRZeax4VICZjYmPS-3ZWAV0XyjjlgWgFleTqVT72M-VlPCdecHiYQJojlYHJyGybvTCaX1cqoF9aAMy8wBvRbSceECmX15k4nKG51Z5Le7k_vOShaxYmwrRhMIip4KRv-DW1FXAdi_F-MYSrqZ6Oq-nglMujxD2NOoHoqOqmyd1OMIrc6oIRuRqBXlRnQ0IdUDQbiXfyFVC0ItIME3a4SLoWp_rrmY1tSrGJu93MZrjhzfkNglJ-FOp4kKZAKkzA"
     # generate the sha256 hash of the encoded header and payload
     message = header + "." + payload
     message = message.encode()
@@ -25,11 +27,11 @@ def verifyJWT(header, payload, sig, pub_key):
     print("SIG")
     print(sig)
 
-    signature_bytes = base64.urlsafe_b64decode(sig)
+    signature_bytes = base64.urlsafe_b64decode(sig + '=' * (4 - len(sig) % 4))
 
-    # res = rsa.verify(hashed_msg, signature_bytes, rsa_pubkey)
-    # print("verified result")
-    # print(res)
+    res = rsa.verify(message, signature_bytes, rsa_pubkey)
+    print("verified result")
+    print(res)
 
 
 def main():
