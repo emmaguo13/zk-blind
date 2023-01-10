a296ee287951481434f78614ac13e3c405e72cde
emmaguo13 <emma@proofofalpha.io>
2023-01-10 13:05:59 -0800

    contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,17 @@ const hre = require("hardhat");
 const secret = require("../secret.json");
 
 async function main() {
+  const verifier = await hre.ethers.getContractFactory("contracts/Verifier.sol:Verifier");
+  const v = await verifier.deploy();
+  const v_d = await v.deployed();
+  console.log(v_d)
+
   const contract = await hre.ethers.getContractFactory("Blind");
 
-  const c = await contract.deploy();
+  const c = await contract.deploy([{
+    verifier: v.address
+  }]
+  );
   await c.deployed();
 }
 
